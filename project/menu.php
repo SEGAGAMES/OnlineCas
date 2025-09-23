0e5a7cee240c3ef348ca0d04d310e9269efb9e39
ZERO-alt-F4 <131106846+ZERO-alt-F4@users.noreply.github.com>
2025-09-23 12:23:24 +1000

    Update menu.php

--- project/menu.php
+++ project/menu.php
@@ -3,8 +3,8 @@
     <ul>
         <li><a href="index.php?page=home" class="<?php echo ($currentPage == 'home') ? 'active' : ''; ?>">Главная</a></li>
         <li><a href="index.php?page=games" class="<?php echo ($currentPage == 'games') ? 'active' : ''; ?>">Игры</a></li>
-        <li><a href="index.php?page=bonuses" class="<?php echo ($currentPage == 'bonuses') ? 'active' : ''; ?>">Бонусы</a></li>
         <li><a href="index.php?page=shop" class="<?php echo ($currentPage == 'shop') ? 'active' : ''; ?>">Магазин</a></li>
+        <li><a href="index.php?page=bonuses" class="<?php echo ($currentPage == 'bonuses') ? 'active' : ''; ?>">Поддержка проекта</a></li>
     </ul>
 </nav>
 
@@ -21,4 +21,5 @@
         include('pages/home.php');
     }
     ?>
+
 </main>
